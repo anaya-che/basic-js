50d3877c106a53ca10c90efb5c0245ff0d85d1d3
Anastasiya Chabatarova <anaya.che22@gmail.com>
2020-03-22 19:55:41 +0300

    fix: fix bugs in simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,31 +2,32 @@ const chainMaker = {
   chainArr: [],
   
   getLength() {
-    return this.chainArr.length;
+    this.chainArr.length;
+    return this;
   },
-
   addLink(value) {
-    this.chainArr.push('( ' + value + ' )');
-    
+    if (value === undefined && value !== null) {
+      value = ' ';
+    }
+    this.chainArr.push(value);
     return this;
   },
-
   removeLink(position) {
-    if (typeof (position) !== 'number' || position <= 0 || position >= this.chainArr.length) { throw new Error()}
+    if (typeof (position) !== 'number' || position <= 0 || position >= this.chainArr.length) {
+      this.chainArr = []; 
+      throw new Error();
+    }
     this.chainArr.splice((position-1), 1);
-    
     return this;
   },
-  
   reverseChain() {
     this.chainArr.reverse();
-
     return this;
   },
-
   finishChain() {
-    let newChain = this.chainArr.map((el, i, arr) => (i < arr.length - 1) ? el +=  '~~' : el);
-    let chainStr = newChain.join('');
+    let chainStr = '';
+    this.chainArr.forEach((el, i, arr) => (i < (arr.length - 1)) ? chainStr += '( ' + el + ' )' + '~~' :  chainStr += '( ' + el + ' )');
+    this.chainArr = [];
     return chainStr;
   }
 };
