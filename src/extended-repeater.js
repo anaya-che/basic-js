9d179431dc906569b70bf314ff1b2a7c35d54911
Anastasiya Chabatarova <anaya.che22@gmail.com>
2020-03-22 17:51:45 +0300

    feat: solve Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,41 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let newstr = [];
+  
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+    }
+
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+
+    if (options.addition === undefined) {
+        options.additionRepeatTimes = '';
+    }
+  
+    for (i = 0; i < options.repeatTimes; i++) {
+        newstr += str;
+    for (j = 0; j < options.additionRepeatTimes; j++) {
+        newstr += options.addition;
+
+        if (j < (options.additionRepeatTimes - 1)) {
+            newstr += options.additionSeparator;
+        }
+    } 
+
+    if (i < (options.repeatTimes - 1)) {
+        newstr+=  options.separator;
+    } 
+}
+
+    return newstr;
 };
   
